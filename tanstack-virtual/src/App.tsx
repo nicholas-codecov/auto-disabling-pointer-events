f5b82a7b3dcb13e0835e86de0c33bef20c24d647
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-10-08 10:05:58 -0300

    setup tanstack-virtual example

--- tanstack-virtual/src/App.tsx
+++ tanstack-virtual/src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { useVirtualizer } from "@tanstack/react-virtual";
+
+import LargeData from "./LargeData.json";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const parentRef = useRef(null);
+  const rowVirtualizer = useVirtualizer({
+    count: LargeData.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 35,
+    overscan: 5,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ display: "grid", placeItems: "center" }}>
+      <h1>TanStack Virtual Example</h1>
+      <div
+        ref={parentRef}
+        className="List"
+        style={{
+          height: `400px`,
+          width: `500px`,
+          overflow: "auto",
+        }}
+      >
+        <div
+          style={{
+            height: `${rowVirtualizer.getTotalSize()}px`,
+            width: "100%",
+            position: "relative",
+          }}
+        >
+          {rowVirtualizer.getVirtualItems().map((virtualRow) => (
+            <div
+              key={virtualRow.index}
+              className="row-styles"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: `${virtualRow.size}px`,
+                transform: `translateY(${virtualRow.start}px)`,
+              }}
+            >
+              Row {virtualRow.index}
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
